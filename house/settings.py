8612e97b60f4c246229f2c0b4107b555c74e6612
bryanmsombra27 <bryanjm96@gmail.com>
2025-08-29 13:47:41 -0600

    adding models

--- house/settings.py
+++ house/settings.py
@@ -84,6 +84,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'btredb',
+        # 'USER': 'postgres',
+        # 'PASSWORD':'123456',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
