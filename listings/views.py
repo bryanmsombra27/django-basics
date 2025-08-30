4534687da8682c3e959681789632bb0d61c28150
bryanmsombra27 <bryanjm96@gmail.com>
2025-08-30 12:23:18 -0600

    adding about functionality

--- listings/views.py
+++ listings/views.py
@@ -5,7 +5,8 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 def index(request):
-    listings = Listing.objects.all().order_by("-list_date")
+    listings = Listing.objects.all().order_by(
+        "-list_date").filter(is_published=True)
     paginator = Paginator(listings, 6)
     page = request.GET.get("page")
     paged_listings = paginator.get_page(page)
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,25 @@
 
 # Create your views here.
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by("-list_date").filter(
+        is_published=True)[:3]
+
+    return render(request, 'pages/index.html', {
+        "listings": listings
+    })
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.all()
+
+    mvp_realtors = realtors.filter(is_mvp=True)
+
+    return render(request, 'pages/about.html', {
+        "realtors": realtors,
+        "mvp_realtors": mvp_realtors
+    })
